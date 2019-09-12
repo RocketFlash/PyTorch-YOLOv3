6c043b5b3107b421af813c0a339f49f5101350b5
Rauf <goodraff@gmail.com>
2019-09-12 05:20:11 +0000

    modify detection

--- detect.py
+++ detect.py
@@ -21,17 +21,27 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.ticker import NullLocator
 
+
+# With GAN
+# python test.py --weights_path checkpoints_gan/yolov3_ckpt_19.pth 
+# --model_def ~/datasets/bdd100k/yolo_files/with_gan/yolov3-bdd100k.cfg 
+# --data_config ~/datasets/bdd100k/yolo_files/with_gan/bdd100k.data 
+# --class_path ~/datasets/bdd100k/yolo_files/with_gan/bdd100k.names 
+# --batch_size=4 --pickle_file_name with_gan.pkl
+
+# python detect.py--image_folder '/home/rauf/datasets/bdd100k/images/test/' --weights_path checkpoints_gan/yolov3_ckpt_19.pth --model_def ~/datasets/bdd100k/yolo_files/with_gan/yolov3-bdd100k.cfg --data_config ~/datasets/bdd100k/yolo_files/with_gan/bdd100k.data --class_path ~/datasets/bdd100k/yolo_files/with_gan/bdd100k.names
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_folder", type=str, default="data/samples", help="path to dataset")
     parser.add_argument("--model_def", type=str, default="config/yolov3.cfg", help="path to model definition file")
     parser.add_argument("--weights_path", type=str, default="weights/yolov3.weights", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="data/coco.names", help="path to class label file")
-    parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
-    parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
+    parser.add_argument("--conf_thres", type=float, default=0.9, help="object confidence threshold")
+    parser.add_argument("--nms_thres", type=float, default=0.5, help="iou thresshold for non-maximum suppression")
     parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
-    parser.add_argument("--n_cpu", type=int, default=0, help="number of cpu threads to use during batch generation")
-    parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
+    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+    parser.add_argument("--img_size", type=int, default=800, help="size of each image dimension")
     parser.add_argument("--checkpoint_model", type=str, help="path to checkpoint model")
     opt = parser.parse_args()
     print(opt)
@@ -53,7 +63,7 @@ if __name__ == "__main__":
     model.eval()  # Set in evaluation mode
 
     dataloader = DataLoader(
-        ImageFolder(opt.image_folder, img_size=opt.img_size),
+        ImageFolder(opt.image_folder, img_size_w=opt.img_size, img_size_h=opt.img_size),
         batch_size=opt.batch_size,
         shuffle=False,
         num_workers=opt.n_cpu,
